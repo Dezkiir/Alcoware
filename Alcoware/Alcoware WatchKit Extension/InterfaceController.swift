51e51cf3b20258532e7247c72e7c767902197f92
Dezkiir <55628660+Dezkiir@users.noreply.github.com>
2022-02-07 12:20:53 +0000

    Alcoware Project
    First Commit with Front End Storyboard

---
+++ Alcoware/Alcoware WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Alcoware WatchKit Extension
+//
+//  Created by Dylan Deakin on 02/01/2022.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
